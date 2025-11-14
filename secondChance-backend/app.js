f5e0eec207392c762efdddbd5d61d4b3dcc509d6
Kirima Kangwanwongsakul <61814979+KK44689@users.noreply.github.com>
2025-11-14 11:24:10 +0700

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -14,9 +14,9 @@ const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB')
+  pinoLogger.info('Connected to DB')
 })
-    .catch((e) => console.error('Failed to connect to DB', e))
+  .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -36,14 +36,14 @@ app.use(pinoHttp({ logger }))
 
 // Global Error Handler
 app.use((err, req, res) => {
-    console.error(err)
-    res.status(500).send('Internal Server Error')
+  console.error(err)
+  res.status(500).send('Internal Server Error')
 })
 
 app.get('/', (req, res) => {
-    res.send('Inside the server')
+  res.send('Inside the server')
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
